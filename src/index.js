813f142e73f4d23483169579605a0de51df4d70f
Julie Judachina <iuliia.saunova@email.cz>
2021-07-18 02:10:22 +0200

    renderOne

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import './css/styles.css';
 import Notiflix from "notiflix";
 const debounce = require('lodash.debounce');
 import getRefs from './js/getRefs.js';
+import { fetchCountries } from "./js/fetchCountries";
+import renderOneCountry from "./teamplates/renderOneCountry.hbs";
+import renderFewCountry from "./teamplates/renderFewCountries.hbs";
+
+
 const refs = getRefs();
 
-
-
 const DEBOUNCE_DELAY = 300;
-
-import { fetchCountries } from "./js/fetchCountries"
 
 
 fetchCountries()
