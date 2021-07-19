f0633c71e4b2e270630ee72bd7fad65db6a417a5
Julie Judachina <iuliia.saunova@email.cz>
2021-07-19 02:08:09 +0200

    done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const debounce = require('lodash.debounce');
 import getRefs from './js/getRefs.js';
 import { fetchCountries } from "./js/fetchCountries";
 import countryCard from "./teamplates/renderOneCountry.hbs";
-import renderCountryList from "./teamplates/renderFewCountries.hbs";
+import CountriesCard from "./teamplates/renderFewCountries.hbs";
 
 
 const refs = getRefs();
@@ -28,12 +28,12 @@ function onSearch(e) {
     else {
 
         fetchCountries(searchLetter)
-            .then(renderCountryCard)
+            .then(CountriesCardMarkup)
             .catch(error => console.log(error))
     }
 }
 
-function renderCountryCard(countries) {
+function CountriesCardMarkup(countries) {
     if (countries.length === 1) {
         refs.countryInfo.insertAdjacentHTML('beforeend', countryCard(countries[0]));
     }
@@ -46,7 +46,7 @@ function renderCountryCard(countries) {
 
     }
     else if (countries.length >= 2 && countries.length <= 10) {
-        refs.countryList.insertAdjacentHTML('beforeend', renderCountryList(countries));
+        refs.countryList.insertAdjacentHTML('beforeend', CountriesCard(countries));
 
     }
 
